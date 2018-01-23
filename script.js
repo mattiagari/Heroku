7379a5261610dd929ecc0e4c2839b2cc0b4beddb
Mattia Gariselli <mdsmatti@gmail.com>
2018-01-23 10:27:21 +0100

    Heroku

---
+++ script.js
@@ -0,0 +1,24 @@
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      myFunction(this);
+    }
+  };
+  xhttp.open("GET", "nutrition.xml", true);
+  xhttp.send();
+}
+function myFunction(xml) {
+  var i;
+  var xmlDoc = xml.responseXML;
+  var table="<tr><th>Cibo</th><th>mfr</th></tr>";
+  var x = xmlDoc.getElementsByTagName("food");
+  for (i = 0; i <x.length; i++) { 
+    table += "<tr><td>" +
+    x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+	 x[i].getElementsByTagName("mfr")[0].childNodes[0].nodeValue +
+    "</td><td>" ;
+  }
+  document.getElementById("demo").innerHTML = table;
+}
